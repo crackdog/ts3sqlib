fa0ac2fcacc5c8eb9dd251c0457cd8aaa66c6825
Florian Kahllund <flo.kahllund@gmail.com>
2014-06-10 13:45:00 +0200

    adding commentary

--- ts3sqlib_escape.go
+++ ts3sqlib_escape.go
@@ -1,10 +1,13 @@
 package ts3sqlib
 
+//Escape escapes a given string as described in the ts3 server query manual.
 func Escape(msg string) string {
 	//TODO escape the msg
 	return msg
 }
 
+//Unescape unescapes a given escaped string as described in the ts3 server
+//query manual.
 func Unescape(escaped_msg string) string {
 	//TODO unescape a escaped message
 	return escaped_msg
