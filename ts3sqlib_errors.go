6594fa5e502ad792c5c15d168e803e86bab7e316
Florian Kahllund <flo.kahllund@gmail.com>
2014-06-10 13:44:06 +0200

    adding ts3sqlib_errors.go

--- ts3sqlib_errors.go
+++ ts3sqlib_errors.go
@@ -5,12 +5,14 @@ import (
 	"strings"
 )
 
+//Error contains additional error information.
 type Error struct {
 	Id       int
 	Msg      string
 	ExtraMsg string
 }
 
+//Error returns the error in a string representation.
 func (err Error) Error() string {
 	s := fmt.Sprintf("error id=%d msg=%s", err.Id, err.Msg)
 	if err.ExtraMsg != "" {
@@ -19,15 +21,18 @@ func (err Error) Error() string {
 	return s
 }
 
+//NewError creates a new Error from an id, message and an extra_message.
 func NewError(id int, msg, extramsg string) Error {
 	return Error{id, msg, extramsg}
 }
 
+//isError tests if a given string is a ts3 server query error
 func isError(line string) bool {
 	return strings.Contains(line, "error") &&
 		strings.Contains(line, "id=") && strings.Contains(line, "msg=")
 }
 
+//toError converts a given string into a ts3 server query error.
 func toError(line string) (err Error) {
 	if !isError(line) {
 		err = Error{666, "line is not an error!", ""}
---
+++ ts3sqlib_escape.go
@@ -0,0 +1,11 @@
+package ts3sqlib
+
+func Escape(msg string) string {
+	//TODO escape the msg
+	return msg
+}
+
+func Unescape(escaped_msg string) string {
+	//TODO unescape a escaped message
+	return escaped_msg
+}
