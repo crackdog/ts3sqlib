02fd376baeca5b0de2c7b167b0173fd9c33b1208
Florian Kahllund <flo.kahllund@gmail.com>
2014-06-10 13:45:43 +0200

    adding some functions and commentary

--- ts3sqlib.go
+++ ts3sqlib.go
@@ -2,6 +2,7 @@ package ts3sqlib
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net"
 	"strings"
@@ -10,16 +11,22 @@ import (
 )
 
 var (
-	timeout = 5 * time.Second
+	timeout = 5 * time.Second //the timeout of the connection
 )
 
+//SqConn contains the connection to a ts3 server.
 type SqConn struct {
-	conn      net.Conn
-	logger    *log.Logger
-	sendMutex *sync.Mutex
-	timeout   time.Duration
+	conn       net.Conn
+	logger     *log.Logger
+	sendMutex  *sync.Mutex
+	timeout    time.Duration
+	receiving  bool
+	recvNotify chan string
+	recvChan   chan string
 }
 
+//Dial creates a new SqConn and connects to the ts3 server with the given
+//address and returns a pointer to it and an error.
 func Dial(address string, logger *log.Logger) (conn *SqConn, err error) {
 	conn = nil
 
@@ -33,18 +40,61 @@ func Dial(address string, logger *log.Logger) (conn *SqConn, err error) {
 	}
 
 	conn = &SqConn{
-		conn:      connection,
-		logger:    logger,
-		sendMutex: &sync.Mutex{},
-		timeout:   1000 * time.Millisecond,
+		conn:       connection,
+		logger:     logger,
+		sendMutex:  &sync.Mutex{},
+		timeout:    1000 * time.Millisecond,
+		receiving:  true,
+		recvNotify: make(chan string),
+		recvChan:   make(chan string),
 	}
+
+	go conn.recv() //goroutine that splits the incoming messages into notify
+	//and normal messages.
+
 	return
 }
 
+func (c *SqConn) recv() {
+	line := ""
+	var err error
+	for c.receiving {
+		//read line
+		line, err = bufio.NewReader(c.conn).ReadString('\n')
+
+		if err != nil {
+			panic(err)
+		}
+
+		//decide if its notify or not and put it to the correct channel
+		if strings.HasPrefix(line, "notify") {
+			c.recvNotify <- line
+		} else {
+			c.recvChan <- line
+		}
+	}
+}
+
+//RecvNotify returns a notify message or blocks until one arrives.
+func (c *SqConn) RecvNotify() (answer string, err error) {
+	answer = ""
+	err = nil
+
+	if c == nil {
+		err = fmt.Errorf("no SqConn")
+		return
+	}
+
+	answer = <-c.recvNotify
+	return
+}
+
+//Close closes the connection to the ts3 server.
 func (c *SqConn) Close() {
 	c.conn.Close()
 }
 
+//Send sends a message to the server and returns the answer.
 func (c *SqConn) Send(msg string) (answer string, err error) {
 	c.sendMutex.Lock()
 	defer c.sendMutex.Unlock()
@@ -62,11 +112,12 @@ func (c *SqConn) Send(msg string) (answer string, err error) {
 	line := ""
 
 	for !isError(line) {
-		line, err = bufio.NewReader(c.conn).ReadString('\n')
+		/*line, err = bufio.NewReader(c.conn).ReadString('\n')
 		if err != nil {
 			return
-		}
+		}*/
 		answer += line
+		line = <-c.recvChan
 	}
 	err = toError(line)
 
