0956a5bc8abf2732727b5401b3720eebc90da775
Florian Kahllund <flo.kahllund@gmail.com>
2014-06-16 21:25:55 +0200

    adding test for Unescape

--- ts3sqlib_escape_test.go
+++ ts3sqlib_escape_test.go
@@ -12,3 +12,12 @@ func TestEscape(t *testing.T) {
 		t.Errorf("Escape(%s) = '%s', want '%s'", a, x, ae)
 	}
 }
+
+func TestUnescape(t *testing.T) {
+	ae := "hallo\\swelt"
+	a := "hallo welt"
+
+	if x := Unescape(ae); x != a {
+		t.Errorf("Unescape(%s) = '%s', want '%s'", ae, x, a)
+	}
+}
