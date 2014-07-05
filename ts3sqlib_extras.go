092f25ef7039ce7e618ddc985de9188144cf67a9
Florian Kahllund <flo.kahllund@gmail.com>
2014-07-05 22:51:55 +0200

    adding datastructure for clients

--- ts3sqlib_extras.go
+++ ts3sqlib_extras.go
@@ -1,8 +1,55 @@
 package ts3sqlib
 
 import (
+	"strconv"
 	"strings"
 )
+
+//Client hold all information for a client from the clientlist command.
+type Client struct {
+	Cid              int    `json:"-"`
+	Clid             int    `json:"clid"`
+	ClientDatabaseId int    `json:"-"`
+	ClientNickname   string `json:"client_nickname"`
+	ClientType       int    `json:"-"`
+}
+
+//NewClient creates a Client datastructure from a map of strings
+func NewClient(cmap map[string]string) Client {
+	var newC Client
+
+	newC.Cid, _ = strconv.Atoi(cmap["cid"])
+	newC.Clid, _ = strconv.Atoi(cmap["clid"])
+	newC.ClientDatabaseId, _ = strconv.Atoi(cmap["client_database_id"])
+	newC.ClientNickname = cmap["client_nickname"]
+	newC.ClientType, _ = strconv.Atoi(cmap["client_type"])
+
+	return newC
+}
+
+//ClientmapsToClients converts an array of string maps to an array of Client's.
+func ClientmapsToClients(clientmaps []map[string]string) (clients []Client, err error) {
+	clients = make([]Client, len(clientmaps))
+
+	for i, clientmap := range clientmaps {
+		clients[i] = NewClient(clientmap)
+	}
+
+	return
+}
+
+//ClientlistToClients gets the clientlist from the ts3 server and returns it as
+//a slice of Client's.
+//The params are described in the TS3 ServerQuery Manual.
+func (c *SqConn) ClientlistToClients(params string) (clients []Client, err error) {
+	clientmaps, err := c.ClientlistToMaps(params)
+	if err != nil {
+		return
+	}
+
+	clients, err = ClientmapsToClients(clientmaps)
+	return
+}
 
 //ClientlistToMaps gets the clientlist from the ts3 server and returns it as
 //a slice of maps.
